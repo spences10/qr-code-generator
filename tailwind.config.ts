f29dbbc72bf6b77300fc067b90cd685422ce955c
spences10 <spences10apps@gmail.com>
2025-01-29 08:40:18 +0000

    Add DaisyUI and update Prettier configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
 	},
 
-	plugins: [typography]
+	plugins: [typography, daisyui],
 } satisfies Config;
